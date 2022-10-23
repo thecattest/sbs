499d5333ae8a6e8154fb2fc5060596a745ae20fa
Владимир Алексеев <it@vladimirva.ru>
2022-10-23 12:39:53 +0300

    check_sms_code imp

--- blueprints/methods.py
+++ blueprints/methods.py
@@ -1,5 +1,6 @@
 import re
 from random import randint
+from datetime import datetime
 
 NUM_CHECK = re.compile(r".*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*")
 NUM_F = re.compile(r'((\+7|7|8)+([0-9]){10})$')
@@ -16,3 +17,13 @@ def check_phone_number(number):
 def generate_sms_code():
     code = randint(1000, 9999)
     return code
+
+
+def check_sms_code(code, expiries_at, u_code):
+    if code is None or u_code is None:
+        return False
+    if code != u_code:
+        return False
+    if datetime.utcnow() > expiries_at:
+        return False
+    return True
