fba68cef7d7c285b3a686996a819d0e9ac4674f3
Владимир Алексеев <valeksee@sfedu.ru>
2022-10-23 11:54:28 +0300

    generate_sms_code imp

--- blueprints/methods.py
+++ blueprints/methods.py
@@ -1,4 +1,5 @@
 import re
+from random import randint
 
 NUM_CHECK = re.compile(r".*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*(\d).*")
 NUM_F = re.compile(r'((\+7|7|8)+([0-9]){10})$')
@@ -10,3 +11,8 @@ def format_phone_number(number):
 
 def check_phone_number(number):
     return bool(NUM_CHECK.match(number))
+
+
+def generate_sms_code():
+    code = randint(1000, 9999)
+    return code
